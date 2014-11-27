c883486178c184b5c71e50c7a46f83fff4a65210
Jin-WU <jinwu@email.arizona.edu>
2014-11-26 18:24:37 -0700

    Updated Readme file with info on scripts

--- Matlab-code/tests for Jin/Matlab Code_For_Loren/Test_Environmental_Variable_Extraction_V1.m
+++ Matlab-code/tests for Jin/Matlab Code_For_Loren/Test_Environmental_Variable_Extraction_V1.m
@@ -16,6 +16,8 @@ fn_output='.\Env_Variables_Output\'; % for Mac user, it should be    fn_output='
 fn_physiogy_output='.\Physiology_Output\'; % for Mac user, it should be  fn_physiogy_output='./Physiology_Output/';
 
 
+
+
 % PART 1: Automatically extract environmental variables 
 
 listing = dir(folder); % navigate to where we locate the master excel spreadsheet
@@ -24,96 +26,97 @@ n1=length(listing); %% How many files within this folder
 % Among n1 files, the first 3 are not .csv files, while the last one is the
 % readme file
 
-% count=0;
-% for i=4:n1 % using the for-loop to automatically search each file
-%     str=listing(i,1); % extracting the file name for each file
-%     str_name=str.name;
-%     if str_name(1,1)=='A' % Only applicable for those ACI-curves
-%         count=count+1;
-%         
-%         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%         % Attention for MAC User  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%         
-%         fn=[folder '\' str_name]; % in MAC, we use fn=[folder '/' str]; 
-%         
-%         
-%         
-%         
-%         [num txt raw]=xlsread(fn); % although it is a csv file, I use xlsread function instead                
-%         Output(count,1)=length(num(:,1)); %% the number of observations
-%         
-%         Output(count,2)=num(1,1); %% the starting of observation number
-%         Output(count,3)=num(end,1); %% the ending of observation number
-%         
-%         Output(count,4)=mean(num(num(:,10)>0,10)); %% mean-conductance
-%         Output(count,5)=std(num(num(:,10)>0,10)); %% std-conductance
-%         
-%         Output(count,6)=mean(num(num(:,20)>0,20)); %% mean-Leaf-Temp
-%         Output(count,7)=std(num(num(:,20)>0,20)); %% std-Leaf-Temp
-%         
-%         Output(count,8)=mean(num(num(:,19)>0,19)); %% mean-Air-Temp
-%         Output(count,9)=std(num(num(:,19)>0,19)); %% std-Air-Temp
-%         
-%         Output(count,10)=mean(num(num(:,29)>0,29)); %% mean-PARi
-%         Output(count,11)=std(num(num(:,29)>0,29)); %% std-PARi
-%         
-%         Output(count,12)=mean(num(num(:,13)>0,13)); %% mean-VPDL
-%         Output(count,13)=std(num(num(:,13)>0,13)); %% std-VPDL
-%         
-%         Output(count,14)=mean(num(num(:,27)>0,27)); %% mean-RH_S
-%         Output(count,15)=std(num(num(:,27)>0,27)); %% std-RH_S
-%         
-%         fn_track(count,1)={str_name};
-%         
-%         clear num txt raw fn 
-%     end
-%     % conductance
-%     % Tleaf
-%     % Tair
-%     % PARi
-%     % VPDL
-%     % Observation matrix, # of observations
-%     % RH_sample 
-%     clear str str_name
-% end
-% 
-% Environment_Mat(1,1)={'filename'};
-% Environment_Mat(1,2)={'# of obs'};
-% Environment_Mat(1,3)={'obs start'};
-% Environment_Mat(1,4)={'obs end'};
-% Environment_Mat(1,5)={'mean cond'};
-% Environment_Mat(1,6)={'std cond'};
-% Environment_Mat(1,7)={'mean LeafT'};
-% Environment_Mat(1,8)={'std LeafT'};
-% Environment_Mat(1,9)={'mean AirT'};
-% Environment_Mat(1,10)={'std AirT'};
-% Environment_Mat(1,11)={'mean PARi'};
-% Environment_Mat(1,12)={'std PARi'};
-% Environment_Mat(1,13)={'mean VPDL'};
-% Environment_Mat(1,14)={'std VPDL'};
-% Environment_Mat(1,15)={'mean RH_S'};
-% Environment_Mat(1,16)={'std RH_S'};
-% 
-% n2=length(Output(:,1));
-% 
-% Environment_Mat(2:n2+1,1)=fn_track; 
-% for i=1:n2
-%     for j=1:15
-%         Environment_Mat(i+1,j+1)={Output(i,j)};
-%     end
-% end
-% 
-% xlswrite([fn_output 'Env_Variable_Master_Sheet.xlsx'],Environment_Mat);
-% clear n1 n2 fn_track Output
-% 
-% save Loren_Environmental_Variable
+count=0;
+for i=4:n1 % using the for-loop to automatically search each file
+    str=listing(i,1); % extracting the file name for each file
+    str_name=str.name;
+    if str_name(1,1)=='A' % Only applicable for those ACI-curves
+        count=count+1;
+        
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Attention for MAC User  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        fn=[folder '\' str_name]; % in MAC, we use fn=[folder '/' str]; 
+        
+        
+        
+        
+        [num txt raw]=xlsread(fn); % although it is a csv file, I use xlsread function instead                
+        Output(count,1)=length(num(:,1)); %% the number of observations
+        
+        Output(count,2)=num(1,1); %% the starting of observation number
+        Output(count,3)=num(end,1); %% the ending of observation number
+        
+        Output(count,4)=mean(num(num(:,10)>0,10)); %% mean-conductance
+        Output(count,5)=std(num(num(:,10)>0,10)); %% std-conductance
+        
+        Output(count,6)=mean(num(num(:,20)>0,20)); %% mean-Leaf-Temp
+        Output(count,7)=std(num(num(:,20)>0,20)); %% std-Leaf-Temp
+        
+        Output(count,8)=mean(num(num(:,19)>0,19)); %% mean-Air-Temp
+        Output(count,9)=std(num(num(:,19)>0,19)); %% std-Air-Temp
+        
+        Output(count,10)=mean(num(num(:,29)>0,29)); %% mean-PARi
+        Output(count,11)=std(num(num(:,29)>0,29)); %% std-PARi
+        
+        Output(count,12)=mean(num(num(:,13)>0,13)); %% mean-VPDL
+        Output(count,13)=std(num(num(:,13)>0,13)); %% std-VPDL
+        
+        Output(count,14)=mean(num(num(:,27)>0,27)); %% mean-RH_S
+        Output(count,15)=std(num(num(:,27)>0,27)); %% std-RH_S
+        
+        fn_track(count,1)={str_name};
+        
+        clear num txt raw fn 
+    end
+    % conductance
+    % Tleaf
+    % Tair
+    % PARi
+    % VPDL
+    % Observation matrix, # of observations
+    % RH_sample 
+    clear str str_name
+end
+
+Environment_Mat(1,1)={'filename'};
+Environment_Mat(1,2)={'# of obs'};
+Environment_Mat(1,3)={'obs start'};
+Environment_Mat(1,4)={'obs end'};
+Environment_Mat(1,5)={'mean cond'};
+Environment_Mat(1,6)={'std cond'};
+Environment_Mat(1,7)={'mean LeafT'};
+Environment_Mat(1,8)={'std LeafT'};
+Environment_Mat(1,9)={'mean AirT'};
+Environment_Mat(1,10)={'std AirT'};
+Environment_Mat(1,11)={'mean PARi'};
+Environment_Mat(1,12)={'std PARi'};
+Environment_Mat(1,13)={'mean VPDL'};
+Environment_Mat(1,14)={'std VPDL'};
+Environment_Mat(1,15)={'mean RH_S'};
+Environment_Mat(1,16)={'std RH_S'};
+
+n2=length(Output(:,1));
+
+Environment_Mat(2:n2+1,1)=fn_track; 
+for i=1:n2
+    for j=1:15
+        Environment_Mat(i+1,j+1)={Output(i,j)};
+    end
+end
+
+xlswrite([fn_output 'Env_Variable_Master_Sheet.xlsx'],Environment_Mat);
+clear n1 n2 fn_track Output
+
+save Loren_Environmental_Variable
 
 
 
 
 
 
-% PART 2: Automatically extract environmental variables 
+% PART 2: Automatically Photosynthesis Parameterization
 
 expansion_rate=1; % if equal to 1, the default version of the code; >1, it will generate bigger boundary than the default
 n1=length(listing);
@@ -128,6 +131,8 @@ for i=4:n1 % using the for-loop to automatically search each file
         % Attention for MAC User  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         
         fn=[folder '\' str_name]; % in MAC, we use fn=[folder '/' str];
+        
+        
         
         
         
--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Code-for-Leaf-Physiology
 The goals for this code include:
 (1) Automatically extract environmental variables for each ACI curve
 (2) Run the batch analysis for extracting key physiological parameters
+
+Script descriptions:
+Test_Environmental_Variable_Extraction_V1.m
+includes Part 1: automatically extract the environmental variables and Part 2: automatically extract physiological parameter
+
+Environmental variable is saved as ./Env_Variables_Output/
+Physiological variables is saved as ./Physiology_Output/
+The internal model and observed A-CI plot is also saved as ./Physiology_Output/
